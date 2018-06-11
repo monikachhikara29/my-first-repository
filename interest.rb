efdbc7b9d6720e2a490182a17b91dfde5db80a85
Monika <monikachhikara18@gmail.com>
2018-06-11 14:43:25 +0530

    My stupid commit

--- interest.rb
+++ interest.rb
@@ -14,12 +14,3 @@ class Interest
     @amount_compound_interest - @amount_simple_interest
   end
 end
-
-puts "Enter principal"
-principal = gets.chomp.to_i
-puts "Enter Time"
-time = gets.chomp.to_f
-interest = Interest.new(principal, time)
-puts interest.simple_interest
-puts interest.compound_interest
-puts interest.difference
